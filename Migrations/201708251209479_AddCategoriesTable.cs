d145d4ed6bc7e5671ae24a18bd85d2f690a00705
David Szorad <david.szorad@live.com>
2017-08-25 14:42:05 +0200

    Added CategoryId to CoursesTable

--- Migrations/201708251209479_AddCategoriesTable.cs
+++ Migrations/201708251209479_AddCategoriesTable.cs
@@ -15,7 +15,10 @@ namespace EntityFrameworkCodeFirst.Migrations
                         Name = c.String(),
                     })
                 .PrimaryKey(t => t.Id);
-            
+
+            // to be able to insert Id into DB change identity to false -> Id = c.Int(nullable: false, identity: false)
+            Sql("INSERT INTO Categories (Name) VALUES ('Web Development')");
+            Sql("INSERT INTO Categories (Name) VALUES ('Programming Languages')");
         }
         
         public override void Down()
