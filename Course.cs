ba68369cd0b8c63810884b8b86b56313738e5365
David Szorad <david.szorad@live.com>
2017-08-25 15:01:58 +0200

    Migrations: Modifying an existing class (new datepublish column, renaming title column to name)

--- Course.cs
+++ Course.cs
@@ -1,13 +1,17 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace EntityFrameworkCodeFirst
 {
+    [Table("Courses")]
     public class Course
     {
         public int Id { get; set; }
-        public string Title { get; set; }
+        public string Name { get; set; }
         public string Description { get; set; }
         public Category Category { get; set; }
+        public DateTime? DatePublished { get; set; }
         public CourseLevel Level { get; set; }
         public float FullPrice { get; set; }
         public Author Author { get; set; }
