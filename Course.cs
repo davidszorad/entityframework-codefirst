c2a1e8c3748ff057ffa80f12737fafac58036a3b
David Szorad <david.szorad@live.com>
2017-09-01 18:44:17 +0200

    Data Annotations Example

--- Course.cs
+++ Course.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace EntityFrameworkCodeFirst
@@ -8,11 +9,24 @@ namespace EntityFrameworkCodeFirst
     public class Course
     {
         public int Id { get; set; }
+
+        [Required]
+        [MaxLength(255)]
         public string Name { get; set; }
+
+        [Required]
+        [MaxLength(2000)]
         public string Description { get; set; }
+
         public CourseLevel Level { get; set; }
+
         public float FullPrice { get; set; }
+
+        [ForeignKey("Author")]
+        public int AuthorId { get; set; }
+
         public Author Author { get; set; }
+
         public IList<Tag> Tags { get; set; }
     }
 }
