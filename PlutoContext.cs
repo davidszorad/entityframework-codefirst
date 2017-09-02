ade539d06afc36bc24922d8fa3a66ce2400ec583
David Szorad <david.szorad@live.com>
2017-09-02 11:02:11 +0200

    Organized Fluent API configurations

--- PlutoContext.cs
+++ PlutoContext.cs
@@ -1,4 +1,5 @@
-﻿using System.Data.Entity;
+﻿using EntityFrameworkCodeFirst.EntityConfigurations;
+using System.Data.Entity;
 
 namespace EntityFrameworkCodeFirst
 {
@@ -15,37 +16,7 @@ namespace EntityFrameworkCodeFirst
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Course>()
-                .Property(c => c.Name)
-                .IsRequired()
-                .HasMaxLength(255);
-
-            modelBuilder.Entity<Course>()
-                .Property(c => c.Description)
-                .IsRequired()
-                .HasMaxLength(2000);
-
-            modelBuilder.Entity<Course>()
-                .HasRequired(c => c.Author)
-                .WithMany(a => a.Courses)
-                .HasForeignKey(c => c.AuthorId)
-                .WillCascadeOnDelete(false);
-
-            modelBuilder.Entity<Course>()
-                .HasMany(c => c.Tags)
-                .WithMany(t => t.Courses)
-                .Map(m => 
-                {
-                    m.ToTable("CourseTags");
-                    m.MapLeftKey("CourseId");
-                    m.MapRightKey("TagsId");
-                });
-
-            modelBuilder.Entity<Course>()
-                .HasRequired(c => c.Cover)
-                .WithRequiredPrincipal(c => c.Course);
-
-            base.OnModelCreating(modelBuilder);
+            modelBuilder.Configurations.Add(new CourseConfiguration());
         }
     }
 }
